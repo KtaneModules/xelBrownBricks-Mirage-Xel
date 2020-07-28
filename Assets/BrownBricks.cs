a8c7e91d4c3f4405202cf5e2d7779125215cd7eb
Mirage <joekingyur@gmail.com>
2020-07-28 14:41:56 +0300

    Module files
    #

---
+++ Assets/BrownBricks.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KModkit;
+using System;
+
+public class BrownBricks : MonoBehaviour {
+    public KMSelectable[] bricks;
+    public MeshRenderer[] brickRenderes;
+    public Material[] brickMats;
+    int[] posLookup = new int[] { 0, 3, 2, 1, 2 };
+    int[] brickLookup = new int[] { 2, 3, 4, 1, 0 };
+    int[][][] answerLookup = new int[][][] {
+    new int[][] {
+    new int[] {0,4},
+    new int[] {2,1,3,2}
+    },
+    new int[][] {
+    new int[] {4,3},
+    new int[] {0,2,1,2}
+    },
+    new int[][] {
+    new int[] {2,0},
+    new int[] {3,4,3,1}
+    },
+    new int[][] {
+    new int[] {1,2},
+    new int[] {3,4,1,0}
+    },
+        new int[][] {
+    new int[] {3,1},
+    new int[] {1,4,0,2}
+    },
+    };
+    string[] loggingNames = new string[] {"Hit", "Question", "Wall", "Ground", "Brick"};
+    int[] brickPos = new int[5];
+    int[][] correctAnswerLookup;
+    public KMBombModule module;
+    public KMAudio sound;
+    int missingBrick;
+    int lookBrick;
+    int answerBrick;
+    int moduleId;
+    static int moduleIdCounter;
+    bool solved;
+    void Awake()
+    {
+        moduleId = moduleIdCounter++;
+     foreach (KMSelectable brick in bricks)
+        {
+            KMSelectable pressedBrick = brick;
+            brick.OnInteract += delegate { pressBrick(pressedBrick); return false; };
+        } 
+    }
+    void Start () {
+        generateBricks();
+        for (int i = 0; i < 4; i++)
+        {
+            brickRenderes[i].material = brickMats[brickPos[i]];
+        }
+        missingBrick = brickPos[4];
+        Debug.LogFormat("[Brown Bricks #{0}] The blocks in order are {1}, {2}, {3}, and {4}." , moduleId, loggingNames[brickPos[0]], loggingNames[brickPos[1]], loggingNames[brickPos[2]], loggingNames[brickPos[3]]);
+        Debug.LogFormat("[Brown Bricks #{0}] The missing block is {1}.", moduleId, loggingNames[missingBrick]);
+        lookBrick = brickLookup[brickPos[posLookup[missingBrick]]];
+        correctAnswerLookup = answerLookup[brickPos[lookBrick]];
+        handleAnswerLookup(correctAnswerLookup);
+        Debug.LogFormat("[Brown Bricks #{0}] The >LOOK> block is {1}.", moduleId, loggingNames[lookBrick]);
+        Debug.LogFormat("[Brown Bricks #{0}] The correct block to press is {1}.", moduleId, loggingNames[answerBrick]);
+    }
+	void generateBricks()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            brickPos[i] = -1;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            int index = UnityEngine.Random.Range(0, 5);
+            while (brickPos[index] != -1)
+            {
+                index = UnityEngine.Random.Range(0, 5);
+            }
+            brickPos[index] = i;
+        }
+    }
+    void handleAnswerLookup(int[][] lookup)
+    {
+        if (missingBrick == lookup[0][0])
+        {
+            answerBrick = lookup[1][0];
+        }
+        else if (lookBrick == lookup[0][1])
+        {
+            answerBrick = lookup[1][0];
+        }
+        else
+        {
+            answerBrick = lookup[1][2];
+        }
+       if (missingBrick == answerBrick)
+        {
+            answerBrick = lookup[1][3];
+        }
+    }
+
+    void pressBrick(KMSelectable brick)
+    {
+        if(!solved)
+        {
+            if (Array.IndexOf(bricks, brick) == answerBrick)
+            {
+                module.HandlePass();
+                solved = true;
+                sound.PlaySoundAtTransform("inspector gadget cartoon intro theme", transform);
+                Debug.LogFormat("[Brown Bricks #{0}] That was correct. Module solved.", moduleId);
+            }
+            else
+            {
+                module.HandleStrike();
+                Debug.LogFormat("[Brown Bricks #{0}] That was incorrect. Strike!", moduleId);
+                for (int i = 0; i < 5; i++)
+                {
+                    brickPos[i] = -1;
+                }
+                    Start();
+            }
+        }
+	}
+}
